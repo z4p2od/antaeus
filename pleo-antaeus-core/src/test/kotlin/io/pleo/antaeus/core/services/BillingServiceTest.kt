de63151386e28aaab527dffd6f3e1f44c92abb19
John Beeblebrox <ioannis@pleo.io>
2023-09-17 23:40:26 +0200

    Added unit tests for Invoice & Billing Services functionality
    Note: Some tests are currently commented out due to unresolved issues with testing using coroutines. These tests will be enabled once the issues are resolved.

--- pleo-antaeus-core/src/test/kotlin/io/pleo/antaeus/core/services/BillingServiceTest.kt
+++ pleo-antaeus-core/src/test/kotlin/io/pleo/antaeus/core/services/BillingServiceTest.kt
@@ -1,21 +1,27 @@
 package io.pleo.antaeus.core.services
 
-import io.mockk.every
-import io.mockk.mockk
-import io.mockk.verify
+import io.mockk.*
+import io.pleo.antaeus.core.exceptions.CurrencyMismatchException
 import io.pleo.antaeus.core.external.EmailService
 import io.pleo.antaeus.core.external.PaymentProvider
 import io.pleo.antaeus.core.external.SlackIntegration
+import io.pleo.antaeus.models.Customer
+import io.pleo.antaeus.models.Invoice
 import io.pleo.antaeus.models.InvoiceStatus
 import kotlinx.coroutines.*
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.test.*
 import org.junit.jupiter.api.AfterEach
 import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.extension.ExtendWith
+
+const val INVOICE_ID = 42
+const val CUSTOMER_ID = 44
 
 
-@ExperimentalCoroutinesApi //todo comment
+@ExperimentalCoroutinesApi //
 class BillingServiceTest {
     //mock dependencies
     private val paymentProvider = mockk<PaymentProvider>()
@@ -49,17 +55,89 @@ class BillingServiceTest {
         // Cleanup the TestCoroutineDispatcher
         testDispatcher.cleanupTestCoroutines()
     }
+
     @Test
-     fun `should not process any invoice`() =testDispatcher.runBlockingTest  {
-        //Arrange
+     fun `should not process any invoice`() = testDispatcher.runBlockingTest {
+
         every { invoiceService.fetchByStatus(any()) } returns emptyList()
 
-        //Act
         billingService.billInvoices(invoiceStatus = InvoiceStatus.PENDING)
-        //Assert
+
         verify(exactly = 0) { paymentProvider.charge(any()) }
         verify(exactly = 0) { invoiceService.updateStatus(any(), any()) }
     }
+/*
+
+    @Test
+     fun `should bill once invoice and update statuses`() = testDispatcher.runBlockingTest {
+
+        val invoice = mockk<Invoice>() {
+            every { id } returns INVOICE_ID
+        }
+        every { invoiceService.fetchByStatus(InvoiceStatus.PENDING) } returns listOf(invoice)
+        every { paymentProvider.charge(invoice) } returns true
+        every { invoiceService.updateStatus(any(), any()) } just runs
+
+        billingService.billInvoices(invoiceStatus = InvoiceStatus.PENDING)
+
+        verify {
+            paymentProvider.charge(invoice)
+            invoiceService.updateStatus(INVOICE_ID, InvoiceStatus.PAID)
+        }
+    }
+
+    @Test
+     fun `should fail when payment povider doesn't charge`() = testDispatcher.runBlockingTest {
+        val invoice = mockk<Invoice>() {
+            every { id } returns INVOICE_ID
+        }
+        every { invoiceService.fetchByStatus(InvoiceStatus.PENDING) } returns listOf(invoice)
+        every { paymentProvider.charge(invoice) } returns false
+
+        billingService.billInvoices(invoiceStatus = InvoiceStatus.PENDING)
+
+        verify {
+            paymentProvider.charge(invoice)
+            invoiceService.updateStatus(INVOICE_ID, InvoiceStatus.FAILED_INSUFFICIENT_BALANCE)
+            emailService.sendBillingFailureEmail(invoice)
+        }
+
+    }
+
+    @Test
+     fun `should set state to Invalid Currency when there's a currency missmatch`() =testDispatcher.runBlockingTest {
+        val invoice = mockk<Invoice>() {
+            every { id } returns INVOICE_ID
+        }
+        val customer = mockk<Customer>(){
+            every {id} returns CUSTOMER_ID
+        }
+        every { paymentProvider.charge(invoice) } throws CurrencyMismatchException(invoice.id, customer.id)
+
+        billingService.billInvoices(invoiceStatus = InvoiceStatus.PENDING)
+
+        verify {
+            paymentProvider.charge(invoice)
+            invoiceService.updateStatus(INVOICE_ID, InvoiceStatus.FAILED_INVALID_CURRENCY)
+            slackIntegration.sendChargingFailureMessage(invoice,any())
+        }
+
+    }
+
+    @Test
+    fun `should bill pending invoices at specific date`() = testDispatcher.runBlockingTest {
+        // Everything inside this block runs in a coroutine scope
+
+        every { billingConfiguration.shouldBillPendingInvoices(any()) } returns true
+
+        billingService.autoBilling()
+
+        verify {
+            billingService.billInvoices(InvoiceStatus.PENDING)
+        }
+    }
+*/
+
 }
 
 
--- pleo-antaeus-core/src/test/kotlin/io/pleo/antaeus/core/services/InvoiceServiceTest.kt
+++ pleo-antaeus-core/src/test/kotlin/io/pleo/antaeus/core/services/InvoiceServiceTest.kt
@@ -1,10 +1,12 @@
 package io.pleo.antaeus.core.services
 
-import io.mockk.every
-import io.mockk.mockk
+import io.mockk.*
 import io.pleo.antaeus.core.exceptions.InvoiceNotFoundException
 import io.pleo.antaeus.data.AntaeusDal
+import io.pleo.antaeus.models.Invoice
+import io.pleo.antaeus.models.InvoiceStatus
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertDoesNotThrow
 import org.junit.jupiter.api.assertThrows
 
 class InvoiceServiceTest {
@@ -18,6 +20,43 @@ class InvoiceServiceTest {
     fun `will throw if invoice is not found`() {
         assertThrows<InvoiceNotFoundException> {
             invoiceService.fetch(404)
+        }
+    }
+
+    @Test
+    fun `will fetch if invoice exists`() {
+        val invoiceMock = mockk<Invoice>()
+        every { invoiceMock.id } returns 42
+        every { dal.fetchInvoice(42) } returns invoiceMock
+
+        assertDoesNotThrow {
+            invoiceService.fetch(invoiceMock.id)
+        }
+    }
+
+    @Test
+    fun `update invoice status should throw InvoiceNotFoundException if the invoice does not exist`() {
+        val invoiceId = 404
+        every { dal.fetchInvoice(invoiceId) } returns null
+
+        assertThrows<InvoiceNotFoundException> {
+            invoiceService.updateStatus(invoiceId, InvoiceStatus.PAID)
+        }
+    }
+
+    @Test
+    fun `update invoice status should update Database if the invoice exists`() {
+        val invoiceMock = mockk<Invoice>()
+
+        every { dal.fetchInvoice(42) } returns invoiceMock
+        every { dal.updateInvoiceStatus(42, InvoiceStatus.PAID) } just runs
+
+        assertDoesNotThrow {
+            invoiceService.updateStatus(42, InvoiceStatus.PAID)
+        }
+
+        verify {
+            dal.updateInvoiceStatus(42, InvoiceStatus.PAID)
         }
     }
 }
